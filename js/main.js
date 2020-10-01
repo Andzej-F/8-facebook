77c7ff8af3c568ab3128454a739b5b679a65030d
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-01 13:25:09 +0300

    try out DOM and innerhtml features

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ import posts from'./data.js';
 import renderPosts from './renderPosts.js';
 
 renderPosts(posts);
+
+const asideDOM = document.querySelector('.messenger');
+
+asideDOM.innerhtlml = '<span>Labas rytas :) </span>';
+
+asideDOM.innerHTML += '<span>Ka tu ?</span>';
