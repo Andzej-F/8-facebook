5c781429d5f35abc8baa0f3f458d4719f1d321a4
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2020-10-03 21:52:50 +0300

    renderHeader renderPosts renderSinglepost

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ import posts from'./data.js';
 import renderPosts from './renderPosts.js';
 
 renderPosts(posts);
+/*
 
+renderPosts
+    for -> renderSinglePost
+        renderHeader
+        renderContent
+            renderContentText
+            renderContentGallery
+        renderFooter
 
-// const asideDOM = document.querySelector('.messenger');
-
+        */
+// const asideDOM = document.querySelector('.messenger')
 // asideDOM.innerhtml = '<span>Labas rytas :) </span>';
-
 // asideDOM.innerHTML += '<span>Ka tu ?</span>';
--- js/renderPosts.js
+++ js/renderPosts.js
@@ -1,33 +1,22 @@
+import renderSinglePost from './renderSinglePost.js';
+
 function renderPosts(data) {
-    // validacija-patikrink ar cia buvo gautas tinkamas
-    // tipas (array siuo atveju) 
-    console.log('Gavau duomenis');
+    // validacija
+
     // logika, kuri generuoja turini
 
     let HTML = '';
 
     for (let i =0; i < data.length; i+= 1 ) {
         const post = data [i];
-        HTML += `<div class="post">
-                    <header>${post.author.name} ${post.author.lastname}</Header>
-                    <div> 
-                        ${post.content.text}
-                    </div>
-                    <footer>
-                        LIKE + COMMENT
-                    </footer>
-                </div>`;
-
-        //console.log(post.author.name);
+        HTML += renderSinglePost(post);
     }
-    console.log();
+    
     // susirandame elementa, kuriame norime perrasyti turini
     const feedDOM = document.querySelector('main');
-    console.log(feedDOM);
-
+    
     // i ta elementa ikeliame HTML kintamojo reiksme
-    console.log(feedDOM.innerHTML);
-
+    // perrasome HTML elemento turini
     feedDOM.innerHTML = HTML;
 }
 
